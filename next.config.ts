4151dacd9330c09c3bcfbeedc878f7dc0437e79b
Media Admin <candumanalliancechurch@gmail.com>
2026-07-17 22:27:55 +0800

    fix: hardcode sanity env variables in next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  env:{
+    NEXT_PUBLIC_SANITY_PROJECT_ID: "1bp0q077",
+    NEXT_PUBLIC_SANITY_DATASET: "production",
+    
+  }
 };
 
 export default nextConfig;
